0b0b1b0973db6a3c3268ef5c050da90bb73085f6
Wes-Isaac <yisehakmitiku@gmail.com>
2022-05-21 18:42:47 +0800

    Add remote database

--- classes/Database.php
+++ classes/Database.php
@@ -7,15 +7,24 @@ class Database
     {
        
 
+        // Development Connection
+        // $db_host = "localhost";
+        // $db_user = "cms";
+        // $db_name = "mart";
+        // $db_password = "F7BH_ZDYSbxUQnQj";
+
         
-        $db_host = "localhost";
-        $db_user = "cms";
-        $db_name = "mart";
-        $db_password = "F7BH_ZDYSbxUQnQj";
         // $db_host = "sql105.epizy.com";
         // $db_user = "epiz_30464183";
         // $db_name = "epiz_30464183_martonline";
         // $db_password = "4MovTbexF5P";
+
+        // Remote Database Connection
+        $db_host = "remotemysql.com";
+        $db_user = "Uw1OWWEkvF";
+        $db_name = "Uw1OWWEkvF";
+        $db_password = "tCJ34G5gjE";
+
         
         $dsn = 'mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=utf8';
        try{
